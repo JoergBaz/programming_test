12c0b1e0fb7dcce71b763b35a10cbe6b15bdfa8b
JoergBaz <baz@DESKTOP-QQLNDJ9.localdomain>
2019-10-12 22:19:59 +0200

    Added some comments and user input for individual messages

--- programming_test.py
+++ programming_test.py
@@ -8,12 +8,18 @@ URI = "ws://echo.websocket.org"
 
 async def communicate_with_uri(send):
     async with websockets.connect(URI) as websocket:
+        #send string to URI
         await websocket.send(send)
+
+        #recieve answer from URI
         recieve = await websocket.recv()
 
     return recieve
 
 
+
+
 if __name__ == "__main__":
-    answer = asyncio.get_event_loop().run_until_complete(communicate_with_uri("test_string"))
-    print(answer)
+    send = input("Please enter string for sending to echo.websocket.org: ")
+    answer = asyncio.get_event_loop().run_until_complete(communicate_with_uri(send))
+    print("echo.websocket.org answered: {}".format(answer))
