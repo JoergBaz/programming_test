04aa39d159aed276d5431fecb09f6c50d4893cf3
JoergBaz <baz@DESKTOP-QQLNDJ9.localdomain>
2019-10-12 22:32:47 +0200

    Read json file, send content to echo.websocket.org and print server answer to terminal

--- programming_test.py
+++ programming_test.py
@@ -3,23 +3,41 @@
 #import packages
 import asyncio      # for async IO
 import websockets   # library for websockets protocoll
+import json         # handle json file
 
 URI = "ws://echo.websocket.org"
 
 async def communicate_with_uri(send):
     async with websockets.connect(URI) as websocket:
-        #send string to URI
+        # send string to URI
         await websocket.send(send)
 
-        #recieve answer from URI
+        # recieve answer from URI
         recieve = await websocket.recv()
 
     return recieve
+
+async def send_json_to_uri(json_file):
+    async with websockets.connect(URI) as websocket:
+        # open json file
+        with open(json_file, "r") as f:
+                obj = json.load(f)
+        
+        # dump json object to string
+        # use websocket protocol for sending json string
+        await websocket.send(json.dumps(obj))
+
+        # recieve answer from URI
+        recieve = await websocket.recv()
+
+        # convert recieved string into list
+        data = json.loads(recieve)
+
+    return data
 
 
 
 
 if __name__ == "__main__":
-    send = input("Please enter string for sending to echo.websocket.org: ")
-    answer = asyncio.get_event_loop().run_until_complete(communicate_with_uri(send))
-    print("echo.websocket.org answered: {}".format(answer))
+    data = asyncio.get_event_loop().run_until_complete(send_json_to_uri("DevicesList.json"))
+    print(data)
