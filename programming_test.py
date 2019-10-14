856177609c221f46d51836c0e148177ae91cea20
JoergBaz <baz@DESKTOP-QQLNDJ9.localdomain>
2019-10-14 08:53:36 +0200

    Small changes in comments and style

--- programming_test.py
+++ programming_test.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-#import packages
+# import packages
 import asyncio                   # for async IO
 import websockets                # library for websockets protocoll
 import json                      # handle json file
@@ -9,8 +9,9 @@ from collections import Counter  # count occurences
 URI = "ws://echo.websocket.org"
 NEEDED_KEYS = {"id", "tadd", "name", "sn"} # required keys in json object
 
+
 async def communicate_with_uri(send):
-    """ Sends string 'send' to 'URI' and returns servers answer """
+    """Sends string 'send' to 'URI' and returns servers answer"""
     async with websockets.connect(URI) as websocket:
         # send string to URI
         await websocket.send(send)
@@ -20,18 +21,20 @@ async def communicate_with_uri(send):
 
     return recieve
 
+
 def has_keys(entry, needed_keys):
-    """ Returns true if a given entry contains all keys, else this function returns false """
+    """Returns true if a given entry contains all keys, else this function returns false"""
     return needed_keys.issubset(
         set(map(str.casefold, entry.keys()))
         )
 
-async def get_occurences(json_file):
-    """ Returns occurences in entry 'category' and prints ID into terminal from the given json file """ 
+
+async def get_category_occurences(json_file):
+    """Returns occurences in entry 'category' and prints ID into terminal from the given json file""" 
     async with websockets.connect(URI) as websocket:
         # open json file
         with open(json_file, "r") as f:
-                json_obj = json.load(f)
+            json_obj = json.load(f)
         
         # dump json object to string
         # use websocket protocol for sending json string
@@ -51,11 +54,10 @@ async def get_occurences(json_file):
             if has_keys(entry, NEEDED_KEYS):
                 occurences.update([entry["category"]]) # update counter 
                 print(f'Read ID: {entry["id"]}') # print "id" to terminal
-                print([entry["category"]])
 
     return occurences
 
 
 if __name__ == "__main__":
-    data = asyncio.get_event_loop().run_until_complete(get_occurences("DevicesList.json"))
+    data = asyncio.get_event_loop().run_until_complete(get_category_occurences("DevicesList.json"))
     print(data)
